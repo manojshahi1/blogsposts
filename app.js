a030895485788d1d25c7f807aa53a2d43d38323e
manojshahi1 <55903807+manojshahi1@users.noreply.github.com>
2021-06-02 19:04:50 +0545

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://Admin_manojshahi123:test123@myapp.pjtwu.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const postSchema = {
   title: String,
